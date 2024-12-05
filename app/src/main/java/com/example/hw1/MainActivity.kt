d508a1d3056bce04ba215ef592b15a4ce55ea4ce
Omer Meler <omermele52@gmail.com>
2024-12-05 22:32:31 +0200

    "falling" images added

--- app/src/main/java/com/example/hw1/MainActivity.kt
+++ app/src/main/java/com/example/hw1/MainActivity.kt
@@ -2,10 +2,107 @@ package com.example.hw1
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import androidx.appcompat.widget.AppCompatImageView
+import androidx.lifecycle.lifecycleScope
+import kotlinx.coroutines.Job
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.isActive
+import kotlinx.coroutines.launch
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var column1: List<AppCompatImageView>
+    private lateinit var column2: List<AppCompatImageView>
+    private lateinit var column3: List<AppCompatImageView>
+
+    private lateinit var playJob: Job
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        findViews()
+        initViews()
+    }
+
+    private fun findViews() {
+        column1 = listOf(
+            findViewById(R.id.main_IMG_row1_col1),
+            findViewById(R.id.main_IMG_row2_col1),
+            findViewById(R.id.main_IMG_row3_col1),
+            findViewById(R.id.main_IMG_row4_col1),
+            findViewById(R.id.main_IMG_row5_col1),
+            findViewById(R.id.main_IMG_row6_col1),
+            findViewById(R.id.main_IMG_row7_col1),
+            findViewById(R.id.main_IMG_row8_col1),
+            findViewById(R.id.main_IMG_row9_col1),
+            findViewById(R.id.main_IMG_row10_col1),
+            findViewById(R.id.main_IMG_row11_col1),
+            findViewById(R.id.main_IMG_row12_col1),
+            findViewById(R.id.main_IMG_row13_col1),
+            findViewById(R.id.main_IMG_row14_col1),
+            findViewById(R.id.main_IMG_row15_col1)
+        )
+        column2 = listOf(
+            findViewById(R.id.main_IMG_row1_col2),
+            findViewById(R.id.main_IMG_row2_col2),
+            findViewById(R.id.main_IMG_row3_col2),
+            findViewById(R.id.main_IMG_row4_col2),
+            findViewById(R.id.main_IMG_row5_col2),
+            findViewById(R.id.main_IMG_row6_col2),
+            findViewById(R.id.main_IMG_row7_col2),
+            findViewById(R.id.main_IMG_row8_col2),
+            findViewById(R.id.main_IMG_row9_col2),
+            findViewById(R.id.main_IMG_row10_col2),
+            findViewById(R.id.main_IMG_row11_col2),
+            findViewById(R.id.main_IMG_row12_col2),
+            findViewById(R.id.main_IMG_row13_col2),
+            findViewById(R.id.main_IMG_row14_col2),
+            findViewById(R.id.main_IMG_row15_col2)
+        )
+        column3 = listOf(
+            findViewById(R.id.main_IMG_row1_col3),
+            findViewById(R.id.main_IMG_row2_col3),
+            findViewById(R.id.main_IMG_row3_col3),
+            findViewById(R.id.main_IMG_row4_col3),
+            findViewById(R.id.main_IMG_row5_col3),
+            findViewById(R.id.main_IMG_row6_col3),
+            findViewById(R.id.main_IMG_row7_col3),
+            findViewById(R.id.main_IMG_row8_col3),
+            findViewById(R.id.main_IMG_row9_col3),
+            findViewById(R.id.main_IMG_row10_col3),
+            findViewById(R.id.main_IMG_row11_col3),
+            findViewById(R.id.main_IMG_row12_col3),
+            findViewById(R.id.main_IMG_row13_col3),
+            findViewById(R.id.main_IMG_row14_col3),
+            findViewById(R.id.main_IMG_row15_col3)
+        )
+    }
+
+    private fun initViews() {
+        startRandomDropping()
+    }
+
+    private fun startRandomDropping() {
+        val columns = listOf(column1, column2, column3)
+        playJob = lifecycleScope.launch {
+            while (isActive) {
+                val randomColumn = columns.random()
+                startFallingObject(randomColumn)
+                delay(3000)
+            }
+        }
+    }
+
+    private fun startFallingObject(column: List<AppCompatImageView>) {
+        playJob = lifecycleScope.launch {
+            for (i in column.indices) {
+                if (i > 0) column[i - 1].visibility = AppCompatImageView.INVISIBLE
+                column[i].visibility = AppCompatImageView.VISIBLE
+                delay(300) // Delay between visibility changes
+            }
+            // Make the last item invisible at the end of the animation
+            column.last().visibility = AppCompatImageView.INVISIBLE
+        }
     }
 }
