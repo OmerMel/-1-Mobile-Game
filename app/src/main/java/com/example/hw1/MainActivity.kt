f5231811e3d3a2669c2345da6ba34bd22dfba84e
Omer Meler <omermele52@gmail.com>
2024-12-10 17:28:35 +0200

    #

--- app/src/main/java/com/example/hw1/MainActivity.kt
+++ app/src/main/java/com/example/hw1/MainActivity.kt
@@ -1,9 +1,15 @@
 package com.example.hw1
 
+import android.annotation.SuppressLint
 import android.os.Bundle
+import android.util.Log
+import android.view.View
 import androidx.appcompat.app.AppCompatActivity
 import androidx.appcompat.widget.AppCompatImageView
 import androidx.lifecycle.lifecycleScope
+import com.example.hw1.utilities.GameManage
+import com.google.android.material.floatingactionbutton.ExtendedFloatingActionButton
+import com.google.android.material.textview.MaterialTextView
 import kotlinx.coroutines.Job
 import kotlinx.coroutines.delay
 import kotlinx.coroutines.isActive
@@ -11,21 +17,40 @@ import kotlinx.coroutines.launch
 
 class MainActivity : AppCompatActivity() {
 
+    private lateinit var main_IMG_hearts: List<AppCompatImageView>
     private lateinit var column1: List<AppCompatImageView>
     private lateinit var column2: List<AppCompatImageView>
     private lateinit var column3: List<AppCompatImageView>
+    private lateinit var catcher: List<AppCompatImageView>
+    private lateinit var leftArrow: ExtendedFloatingActionButton
+    private lateinit var rightArrow: ExtendedFloatingActionButton
+    private lateinit var main_LBL_score: MaterialTextView
 
+    private var currentCatcherIndex = 0
     private lateinit var playJob: Job
+    private lateinit var gameManager: GameManage
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
         findViews()
+        gameManager = GameManage(main_IMG_hearts.size)
         initViews()
     }
 
+    @SuppressLint("CutPasteId")
     private fun findViews() {
+        leftArrow = findViewById(R.id.main_FAB_left_arrow)
+        rightArrow = findViewById(R.id.main_FAB_right_arrow)
+        main_LBL_score = findViewById(R.id.main_LBL_score)
+
+        main_IMG_hearts = listOf(
+            findViewById(R.id.main_IMG_heart1),
+            findViewById(R.id.main_IMG_heart2),
+            findViewById(R.id.main_IMG_heart3)
+        )
+
         column1 = listOf(
             findViewById(R.id.main_IMG_row1_col1),
             findViewById(R.id.main_IMG_row2_col1),
@@ -77,11 +102,52 @@ class MainActivity : AppCompatActivity() {
             findViewById(R.id.main_IMG_row14_col3),
             findViewById(R.id.main_IMG_row15_col3)
         )
+        catcher = listOf(
+            findViewById(R.id.main_IMG_row13_col1), // left
+            findViewById(R.id.main_IMG_row13_col2), // center
+            findViewById(R.id.main_IMG_row13_col3) // right
+        )
     }
 
     private fun initViews() {
         startRandomDropping()
+        refreshUI()
+
+
+        leftArrow.setOnClickListener { v -> moveCatcherLeft() }
+        rightArrow.setOnClickListener { v -> moveCatcherRight() }
     }
+
+    private fun checkCollision() {
+        val columns = listOf(column1, column2, column3)
+
+    }
+
+    private fun refreshUI() {
+        if (gameManager.isGameOver) { // Lost!
+            Log.d("Game Status", "Game Over! " + gameManager.score)
+            //changeActivity("😭Game Over!", gameManager.score)
+        } else {
+            if(gameManager.injuries != 0)
+                main_IMG_hearts[main_IMG_hearts.size - gameManager.injuries].visibility = View.INVISIBLE
+            main_LBL_score.text = gameManager.score.toString()
+        }
+    }
+
+    private fun moveCatcher(direction: Int) {
+        catcher[currentCatcherIndex].apply {
+            visibility = AppCompatImageView.INVISIBLE
+            setImageResource(R.drawable.cucumber)
+        }
+        currentCatcherIndex = (currentCatcherIndex + direction + catcher.size) % catcher.size
+        catcher[currentCatcherIndex].apply {
+            setImageResource(R.drawable.catcher)
+            visibility = AppCompatImageView.VISIBLE
+        }
+    }
+
+    private fun moveCatcherRight() = moveCatcher(1)
+    private fun moveCatcherLeft() = moveCatcher(-1)
 
     private fun startRandomDropping() {
         val columns = listOf(column1, column2, column3)
---
+++ app/src/main/java/com/example/hw1/utilities/GameManager.kt
@@ -0,0 +1,22 @@
+package com.example.hw1.utilities
+
+import android.util.Log
+import androidx.appcompat.widget.AppCompatImageView
+
+class GameManage(private val lifeCount: Int = 3) {
+    var score: Int = 0
+        private set
+
+    var currentCatcherIndex: Int = 0
+        private set
+
+    var injuries: Int = 0
+        private set
+
+    val isGameOver: Boolean
+        get() = injuries == lifeCount
+
+    fun checkCollision(column: List<AppCompatImageView>, currentCatcherIndex: Int) {
+
+    }
+}
