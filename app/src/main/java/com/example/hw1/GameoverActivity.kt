6c41cc53c4a3da85789e9e81dcd27b198825f9a9
Omer Meler <Omer.Meler@s.afeka.ac.il>
2024-12-12 18:34:21 +0200

    Adjustable text size

--- app/src/main/java/com/example/hw1/GameoverActivity.kt
+++ app/src/main/java/com/example/hw1/GameoverActivity.kt
@@ -1,7 +1,6 @@
 package com.example.hw1
 
 import android.os.Bundle
-import android.os.PersistableBundle
 import androidx.appcompat.app.AppCompatActivity
 import com.google.android.material.textview.MaterialTextView
 
